4364c16f6ea4985bace52185ff54668c52094c74
nextlogixsmovie <91918567+nextlogixsmovie@users.noreply.github.com>
2021-10-05 04:26:38 +0500

    Update Hero.vue

--- components/Hero.vue
+++ components/Hero.vue
@@ -4,7 +4,7 @@
 		<div class="text-container">
 			<div class="text">
 				<span class="mini-heading">Now Stremaing</span>
-				<h1><span>Now</span> Streaming</h1>
+				<h1>Nexlogixs Movie App</h1>
 				<a href="#movie-grid" class="button">View Movies</a>
 			</div>
 		</div>
